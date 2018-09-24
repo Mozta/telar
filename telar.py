45ea1cb30881fc48d18ed5ca4a4b6920510fa12d
mozta <viper_rafa_@hotmail.com>
2018-09-24 18:06:06 -0500

    New file in telar

--- telar.py
+++ telar.py
@@ -22,12 +22,56 @@ def binariza(source):
     nueva_imagen = Image.new('L', (160,160), (255))
     nueva_imagen.putdata(datos_binarios)
 
+def patrones(n):
+    z = n
+    t1 = np.zeros((n,n), dtype=np.uint8)
+    t1 = t1.tolist()
+    patrones = []
+    patrones1 = []
+    p = t1
+
+    m = 0
+    for j in range(n-1):
+        for i in reversed(range(n)):
+            t1[m][i] = 255
+            m += 3 if i == n/2 else 2
+            if m > n-1:
+                m = m - n
+        patrones.append(t1)
+        p = np.append(p, t1,0)
+        if(n%2!=0):
+            m+=1
+
+    for i in range(n-1):
+        patrones1.append(p[z:z+n])
+        z+=n
+    return patrones1
+
+def promedios(n,data):
+    promedios = []
+    data_format = np.array(data).reshape(160,160)
+    p = r = 0
+
+    for k in range(int(160/n)):
+        b = []
+        for j in range(0,n):
+            for i in range(p,p+n):
+                b.append(data_format[j][i])
+        c = np.array(b).reshape(n,n)
+        promedios.append(c.mean())
+        print(c)
+        print(c.mean())
+        p+=n
+    print("Soy p: "+str(p))
+    #print(len(promedios))
+    return promedios
+
+
+#Cargar imagen
 img = load_image("lenna.png")
-
+#Pasarla a BN
 img_ = convert_image(img)
-
-img_.show()
-#im.save("3.bmp")
-
-datos = list(img_.getdata())
-print(datos)
+#Genrarar patrones
+patrones(5)
+#Calcular promedios
+print(promedios(5,list(img_.getdata())))
